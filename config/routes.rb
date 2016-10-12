d80f54793db8a64aa903cabf6d4a7f3607a612f1
khaled muamar <khaled@khaleds-MacBook-Pro.local>
2016-10-12 14:55:28 +0800

    delete questions fiels

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,5 @@ Rails.application.routes.draw do
   get "/sign_up" => "clearance/users#new", as: "sign_up"
   root "home#welcome"
   resources :teams
-  # resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
