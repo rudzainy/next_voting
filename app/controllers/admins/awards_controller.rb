90898abf36309baf3fcece4eb69e4d0e530670ba
khaled muamar <khaled@khaleds-MacBook-Pro.local>
2016-10-12 22:28:10 +0800

    remove awards from admin && creat it under admins

--- app/controllers/admins/awards_controller.rb
+++ app/controllers/admins/awards_controller.rb
@@ -1,4 +1,4 @@
-class Admin::AwardsController < ApplicationController
+class Admins::AwardsController < ApplicationController
   def new
   end
 
--- test/controllers/admins/awards_controller_test.rb
+++ test/controllers/admins/awards_controller_test.rb
@@ -1,38 +1,38 @@
 require 'test_helper'
 
-class Admin::AwardsControllerTest < ActionDispatch::IntegrationTest
+class Admins::AwardsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get admin_awards_new_url
+    get admins_awards_new_url
     assert_response :success
   end
 
   test "should get create" do
-    get admin_awards_create_url
+    get admins_awards_create_url
     assert_response :success
   end
 
   test "should get show" do
-    get admin_awards_show_url
+    get admins_awards_show_url
     assert_response :success
   end
 
   test "should get index" do
-    get admin_awards_index_url
+    get admins_awards_index_url
     assert_response :success
   end
 
   test "should get edit" do
-    get admin_awards_edit_url
+    get admins_awards_edit_url
     assert_response :success
   end
 
   test "should get update" do
-    get admin_awards_update_url
+    get admins_awards_update_url
     assert_response :success
   end
 
   test "should get destroy" do
-    get admin_awards_destroy_url
+    get admins_awards_destroy_url
     assert_response :success
   end
 
