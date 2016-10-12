5eb84e79ef24a1c5d244999d9cd183b2b083fc1d
rudzainy <rudzainy@gmail.com>
2016-10-12 16:44:54 +0800

    User can add notes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
 
+  has_many :notes
+
   include Clearance::User
 
   enum role: {user: "0", admin: "1"}
+
+  def added_note?(team_id)
+    return false if Note.find_by(user_id: self.id, team_id: team_id).nil?
+    true
+  end
+
+  def find_team_note_id(team_id)
+    Note.find_by(user_id: self.id, team_id: team_id).id
+  end
 end
