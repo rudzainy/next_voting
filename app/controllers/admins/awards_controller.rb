61a150f21cb004d1f74c61fe02350aba38bdcbde
khaled muamar <khaled@khaleds-MacBook-Pro.local>
2016-10-12 22:55:01 +0800

    admin can create awards

--- app/controllers/admins/awards_controller.rb
+++ app/controllers/admins/awards_controller.rb
@@ -1,14 +1,26 @@
 class Admins::AwardsController < ApplicationController
+
+  before_action :set_award, only: [:show, :edit, :update, :destroy]
+
   def new
+    @award = Award.new
   end
 
   def create
+    @award = Award.new(award_params)
+
+    if @award.save
+      redirect_to admins_award_path(@award)
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def index
+    @awards = Award.all
   end
 
   def edit
@@ -18,5 +30,15 @@ class Admins::AwardsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def award_params
+    params.require(:award).permit(:title)
+  end
+
+  def set_award
+    @award = Award.find(params[:id])
   end
 end
