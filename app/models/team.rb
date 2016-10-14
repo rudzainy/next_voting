c6e7a4f9547db8a3f1c1ec87097d970e52d19fad
rudzainy <rudzainy@gmail.com>
2016-10-14 08:42:22 +0800

    Kevin's magic for voting index

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,9 @@ class Team < ApplicationRecord
 
   mount_uploader :image, ImageUploader
   has_many :notes
+  has_many :votes
 
+  scope :award_vote_count, -> (award_id){
+    where('votes.award_id = ?', award_id).select('teams.id, teams.name, count(votes.id) AS vote_count').distinct.group('teams.id').order('vote_count desc')
+  }
 end
